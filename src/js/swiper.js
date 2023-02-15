5076f57f6dbbd5c239cf54a3c2d79da7c667ad3d
aleixcaasas <acasasbarco@gmail.com>
2023-02-15 17:03:16 +0100

    Swiper file created

---
+++ src/js/swiper.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    loop: false,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        50: {
+            slidesPerView: 2,
+        },
+    },
+});
